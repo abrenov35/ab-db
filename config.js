8610464821861c7099a365e5c1dc0a0342fe2e80
abrenov35 <abrenov35@gmail.com>
2026-09-01 12:13:30 +0200

    Connect AB DB frontend to Apps Script web app

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.ABDB_CONFIG = {
-  // À remplacer après le déploiement du nouveau Web App Apps Script.
-  API_URL: ""
+  API_URL: "https://script.google.com/macros/s/AKfycbx3WxWC-GuwmYUaB99Wi3LQ3-DAUZtG6CJcTLp2entOd8PN5vz-251Lh20TEE_uA40O/exec"
 };
